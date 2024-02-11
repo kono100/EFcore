45007b77122f7fc6819ff831359c9d7e05cfc880
Thiago Campos <thiago-kono@hotmail.com>
2024-02-10 22:36:16 -0300

    add função que fala o que voce disse

--- teste.py
+++ teste.py
@@ -1,6 +1,8 @@
 import speech_recognition as sr
 from tkinter import *
 import threading
+from gtts import gTTS # 2.4.0
+
 
 frase = ""
 
@@ -14,6 +16,33 @@ def reconhecer_fala():
             audio = microfone.listen(source, timeout=10)  # Defina um timeout adequado
             frase = microfone.recognize_google(audio, language='pt-BR')
             print("Você disse: " + frase)
+            
+            # INICIO QUE FALA
+
+            from gtts import gTTS
+            import os
+            import pygame
+
+
+            tts = gTTS(text=frase, lang='pt')  # 'pt' para o idioma português
+            tts.save("output.mp3")
+
+            # Inicializa o mixer Pygame
+            pygame.mixer.init()
+            sound = pygame.mixer.Sound("output.mp3")
+
+            # Reproduz o áudio
+            sound.play()
+
+            # Aguarda a reprodução terminar
+            while pygame.mixer.get_busy():
+                pygame.time.Clock().tick(10)
+
+            # Remove o arquivo de áudio
+            os.remove("output.mp3")
+
+            # FIM DA PARTE QUE FALA
+
         except sr.WaitTimeoutError:
             print("Tempo de espera excedido. Tente novamente.")
         except sr.UnknownValueError:
